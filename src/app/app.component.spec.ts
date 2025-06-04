dbe6b755b62af493796571e1f4ce859a5f122bb9
yunioseijo <yunioseijo@gmail.com>
2025-06-04 10:14:01 +0200

    refactor: update AppComponent tests to use NavbarComponentMock and improve structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import NavbarComponent from './shared/components/navbar.component';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let compile: HTMLElement;
   let app: AppComponent;
+  @Component({
+    selector: 'app-navbar',
+    standalone: true,
+  })
+  class NavbarComponentMock{
+
+  }
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-      providers: [provideRouter([])],
-    }).compileComponents();
+    TestBed.overrideComponent(AppComponent, {
+      set: {
+        imports: [NavbarComponentMock],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      },
+    });
+    // ! Recomendado
+    // await TestBed.configureTestingModule({
+    //   imports: [AppComponent],
+    //   providers: [provideRouter([])],
+    // })
+    // .overrideComponent(AppComponent, {
+    //   add: {
+    //     imports: [NavbarComponentMock]
+    //   },
+    //   remove: {
+    //     imports: [NavbarComponent]
+    //   }
+    // })
+    // .compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     compile = fixture.nativeElement as HTMLElement;
@@ -29,6 +53,4 @@ describe('AppComponent', () => {
     expect(compile.querySelector('app-navbar')).toBeTruthy();
     expect(compile.querySelector('router-outlet')).toBeTruthy();
   });
-
-
 });
