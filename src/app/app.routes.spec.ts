256657cd584ab459a14f238b5ca8deb2313b9d69
yunioseijo <yunioseijo@gmail.com>
2025-06-04 10:27:13 +0200

    test: add unit tests for app routing functionality

---
+++ src/app/app.routes.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing';
+import { routes } from './app.routes';
+import { provideRouter, Router } from '@angular/router';
+import { Location } from '@angular/common';
+describe('App routes', () => {
+  let router: Router;
+  let location: Location
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideRouter(routes)],
+    });
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+
+
+  });
+
+  it('should navigate to "about" redirects to "/about" ', async() => {
+    await router.navigate(['about']);
+    expect(location.path()).toBe('/about');
+  });
+  it('should navigate to "pokemons/page/1" redirects to "/pokemons/page/1" ', async() => {
+    await router.navigate(['pokemons/page/1']);
+    expect(location.path()).toBe('/pokemons/page/1');
+  });
+  it('should navigate to "unknown-page" redirects to "/about" ', async() => {
+    await router.navigate(['unknown-page']);
+    expect(location.path()).toBe('/about');
+  });
+});
